ed9332108fd9fce603ef9914070112c2e68ac189
holzpfanne <el20b025@technikum-wien.at>
2020-12-12 12:37:20 +0100

    typo

--- project/vhdl/io_ctrl_rtl.vhd
+++ project/vhdl/io_ctrl_rtl.vhd
@@ -30,7 +30,7 @@ use IEEE.std_logic_arith.all;
 --         
 -- led_i   :  in std_logic_vector(15 downto 0)
 
-architecture rtl of calc is
+architecture rtl of io_ctrl is
 
     type button_array is array (0 to 3, 0 to N-1) of std_logic;
     signal buttons : button_array;
@@ -39,9 +39,33 @@ architecture rtl of calc is
     signal switchs : switch_array;
 
     signal digit_select : unsigned(1 downto 0);
+
+    signal counter : unsigned(14 downto 0);
+    signal post_clk_digits : std_logic;
+    signal post_clk_debounds : std_logic;
     
 begin
+    clock_scaler:process(clk_i, reset_i)
+    begin
+        if reset_i = '1' then
+            counter <= (others => '0');
+        elsif clk_i'event and clk_i = '1' then
+            counter <= counter + 1;
+            if counter = 0 then
+                post_clk_digits <= '1';
+            else 
+                post_clk_digits <= '0';
+            end if;
 
+            if counter(3) = '0' then
+                post_clk_debounds <= '1';
+            else
+                post_clk_debounds <= '0';
+            end if;
+        end if;
+
+    end process clock_scaler;
+    
     led:process(clk_i, reset_i)
     begin
         if reset_i = '1' then
@@ -53,14 +77,14 @@ begin
 
     end process led;
 
-    button_debouncing:process(clk_i, reset_i)
+    button_debouncing:process(post_clk_debounds, reset_i)
         variable button_low : std_logic;
         variable button_high : std_logic;
     begin
         if reset_i = '1' then
             pbsync_o <= "0000";
             buttons <= ((others=>(others=>'0')));
-        elsif clk_i'event and clk_i = '1' then
+        elsif post_clk_debounds'event and post_clk_debounds = '1' then
             -- shift button state in beginning of reg
             shift_buttons : for i in 0 to 3 loop
                 shift : for j in 0 to N-2 loop
@@ -86,20 +110,20 @@ begin
         end if;
     end process button_debouncing;
 
-    switch_debouncing:process(clk_i, reset_i)
+    switch_debouncing:process(post_clk_debounds, reset_i)
         variable switch_low : std_logic;
         variable switch_high : std_logic;
     begin
         if reset_i = '1' then
             swsync_o <= "0000000000000000";
             switchs <= ((others=>(others=>'0')));
-        elsif clk_i'event and clk_i = '1' then
+        elsif post_clk_debounds'event and post_clk_debounds = '1' then
             -- shift switch state in beginning of reg
             shift_switches : for i in 0 to 15 loop
                 shift : for j in 0 to N-2 loop
                     switchs(i, j+1) <= switchs(i, j);
                 end loop;
-                switchs(i, 0) <= pb_i(i);
+                switchs(i, 0) <= sw_i(i);
             end loop;
 
             for i in 0 to 15 loop
@@ -118,34 +142,34 @@ begin
         end if;
     end process switch_debouncing;
 
-    digits_write:process(clk_i, reset_i)
+    digits_write:process(post_clk_digits, reset_i)
     begin
         if reset_i = '1' then
             digit_select <= "00";
-            ss_sel_o <= "0000";
+            ss_sel_o <= not "0000";
             ss_o <= "00000000";
-        elsif clk_i'event and clk_i = '1' then
-            -- write digit to selects section
+        elsif post_clk_digits'event and post_clk_digits = '1' then
+                -- write digit to selects section
             if digit_select = 1 then
-                ss_sel_o <= "0010";
-                ss_o <= dig1_i;
+                ss_sel_o <= not "0010";
+                ss_o <= not dig1_i;
             elsif digit_select = 2 then
-                ss_sel_o <= "0100";
-                ss_o <= dig2_i;
+                ss_sel_o <= not "0100";
+                ss_o <= not dig2_i;
             elsif digit_select = 3 then
-                ss_sel_o <= "1000";
-                ss_o <= dig2_i;
+                ss_sel_o <= not "1000";
+                ss_o <= not dig3_i;
             else 
-            ss_sel_o <= "0001";
-            ss_o <= dig0_i;
+                ss_sel_o <= not "0001";
+                ss_o <= not dig0_i;
             end if;
 
-            -- increment digit_select if 3 + 1 -> 0 intendet
+            -- increment digit_select if 3 + 1 -> 0 : intendet
             digit_select <= digit_select + 1;
-            --if digit_select == 4 then
-            --    digit_select <= 0;
-            --end if;
+            
         end if;
     end process digits_write;
+
+
    
 end rtl;
--- project/vhdl/io_ctrl_rtl_cfg.vhd
+++ project/vhdl/io_ctrl_rtl_cfg.vhd
@@ -4,7 +4,7 @@
 --  
 -------------------------------------------------------------------------------
 
-configuration calc_rtl_cfg of calc is
+configuration calc_rtl_cfg of io_ctrl is
   for rtl        -- architecture rtl is used for entity data_bus
   end for;
 end calc_rtl_cfg;
